37ff3ec0012035c418efb5fb90f98e59abdba730
BingchuanLiu <136744612+BingchuanLiu@users.noreply.github.com>
2025-05-17 05:37:12 +0000

    Deploying to gh-pages from @ BingchuanLiu/bingchuanliu.github.io@3ce5ad75fe1dc3f06908abd2374d6e701d009946 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,12 +41,7 @@ ninja.data = [{
       },{id: "news-i-will-be-presenting-my-poster-at-the-bridge-stim-workshop-at-the-university-of-pittsburg-on-may-19-20-sparkles-smile",
           title: 'I will be presenting my poster at the BRIDGE-STIM Workshop at the University...',
           description: "",
-          section: "News",},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
+          section: "News",},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
